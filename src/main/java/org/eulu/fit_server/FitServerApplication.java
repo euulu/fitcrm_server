071e8056b0e6c47cdfb6809289dc92c6cab468ec
eulu <anegamega@gmail.com>
2025-02-24 16:12:49 +0200

    Init

---
+++ src/main/java/org/eulu/fit_server/FitServerApplication.java
@@ -0,0 +1,13 @@
+package org.eulu.fit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eulu/fit_server/FitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eulu.fit_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
