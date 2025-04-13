16280b98b3c53f5c647bbc9d0f337c824e28bad8
eulu <anegamega@gmail.com>
2025-04-13 19:10:40 +0300

    Add Trainer entity

---
+++ src/main/java/org/eulu/fit_crm/model/Trainer.java
@@ -0,0 +1,23 @@
+package org.eulu.fit_crm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "trainers")
+public class Trainer {
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String biography;
+    @Lob
+    private byte[] picture;
+}
